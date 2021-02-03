d67c93f8dddd94fced618234da8aac74f4839545
Quan Viet Pham <35153133+vietquanpham@users.noreply.github.com>
2021-02-03 08:44:04 +0700

    Changed app secret key - PART 1 DONE

--- project/server/config.py
+++ project/server/config.py
@@ -8,7 +8,7 @@ database_name = 'diagnostic'
 
 class BaseConfig:
     """Base configuration."""
-    SECRET_KEY = os.getenv('SECRET_KEY', 'diagnostic_secret')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'app_secret')
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
